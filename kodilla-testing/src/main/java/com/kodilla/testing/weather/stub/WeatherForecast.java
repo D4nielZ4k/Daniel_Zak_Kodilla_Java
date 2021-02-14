73055dcdf0efeecfb3be773226d20d03a2ca354d
Daniel Zak <D4nielZ4k@gmail.com>
2021-02-14 20:31:46 +0100

    Next

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/weather/stub/WeatherForecast.java
@@ -0,0 +1,56 @@
+package com.kodilla.testing.weather.stub;
+
+import java.util.*;
+
+public class WeatherForecast {
+    private Temperatures temperatures;
+
+    public WeatherForecast(Temperatures temperatures) {
+        this.temperatures = temperatures;
+    }
+
+    public Map<String, Double> calculateForecast() {
+        Map<String, Double> resultMap = new HashMap<>();
+
+        for (Map.Entry<String, Double> temperature : temperatures.getTemperatures().entrySet()) {
+
+            // adding 1 celsius degree to current value
+            // as a temporary weather forecast
+            resultMap.put(temperature.getKey(), temperature.getValue() + 1.0); // [1]
+
+        }
+        return resultMap;
+
+    }
+
+
+    public double averageTemperatures() {
+        List<Double> temperatureList = new ArrayList<>();
+        double sum = 0;
+        for (Map.Entry<String, Double> temperature : temperatures.getTemperatures().entrySet()) {
+            temperatureList.add(temperature.getValue());
+
+        }
+        for (int i = 0; i<temperatureList.size(); i++){
+            sum += temperatureList.get(i);
+        }
+        return sum/temperatureList.size();
+    }
+
+    public double medianTemperatures() {
+        List<Double> temperatureList = new ArrayList<>();
+
+        for (Map.Entry<String, Double> temperature : temperatures.getTemperatures().entrySet()) {
+            temperatureList.add(temperature.getValue());
+        }
+        Collections.sort(temperatureList);
+
+        for (int i = 0; i<temperatureList.size(); i++){
+            if(temperatureList.size()%2 == 0){
+                int temp = temperatureList.size() /2;
+                return (temperatureList.get(temp)  + temperatureList.get(temp-1))/2;
+            }
+        }
+        return temperatureList.get(temperatureList.size() /2 );
+    }
+}
---
+++ kodilla-testing/src/test/java/com/kodilla/testing/weather/mock/WeatherForecastTestSuite.java
@@ -0,0 +1,80 @@
+package com.kodilla.testing.weather.mock;                                        // [1]
+
+import com.kodilla.testing.weather.stub.Temperatures;                            // [2]
+import com.kodilla.testing.weather.stub.WeatherForecast;                         // [3]
+import org.junit.jupiter.api.Assertions;                                         // [4]
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;                                               // [5]
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.HashMap;                                                        // [6]
+import java.util.Map;                                                            // [7]
+
+import static org.mockito.Mockito.mock;                                          // [8]
+import static org.mockito.Mockito.when;                                          // [9]
+@ExtendWith(MockitoExtension.class)
+class WeatherForecastTestSuite {
+
+   public static Map<String, Double> temperaturesMap = new HashMap<>();
+
+
+    @BeforeEach
+    public void beforeEveryTest() {
+
+        System.out.println("Preparing to execute test " );
+    }
+    @BeforeAll
+    public static void beforeAllTest(){
+        temperaturesMap.put("Rzeszow",10.0);
+        temperaturesMap.put("Krakow", 10.0);
+        temperaturesMap.put("Wroclaw", 10.0);
+        temperaturesMap.put("Warszawa", 10.0);
+        temperaturesMap.put("Gdansk", 10.0);
+    }
+    @Mock
+    private Temperatures temperaturesMock;
+    @Test
+    void testCalculateForecastWithMock() {
+
+       // Given
+        when(temperaturesMock.getTemperatures()).thenReturn(temperaturesMap);
+        WeatherForecast weatherForecast = new WeatherForecast(temperaturesMock);
+
+        //When
+        int quantityOfSensors = weatherForecast.calculateForecast().size();
+
+        //Then
+        Assertions.assertEquals(5, quantityOfSensors);
+    }
+    @Test
+    void testCalculateAverageTemperature(){
+
+        //Given
+
+        when(temperaturesMock.getTemperatures()).thenReturn(temperaturesMap);
+        WeatherForecast weatherForecast = new WeatherForecast(temperaturesMock);
+
+        //When
+        Double result = weatherForecast.averageTemperatures();
+
+        //Then
+        Assertions.assertEquals(10,result);
+    }
+    @Test
+    void test_Calculate_Median_Temperatures() {
+
+        //Given
+
+        when(temperaturesMock.getTemperatures()).thenReturn(temperaturesMap);
+        WeatherForecast weatherForecast = new WeatherForecast(temperaturesMock);
+
+        //When
+        Double result = weatherForecast.medianTemperatures();
+
+        //Then
+        Assertions.assertEquals(10, result);
+    }
+}
