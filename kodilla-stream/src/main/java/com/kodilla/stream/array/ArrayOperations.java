47dfa420202d493f8ab33dd225b5202052a4a39a
Daniel Zak <D4nielZ4k@gmail.com>
2021-02-22 20:22:57 +0100

    commit

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -2,14 +2,25 @@ package com.kodilla.stream.array;
 
 import java.util.stream.IntStream;
 
-public interface ArrayOperations {
-    static  double getAverage(int[] numbers){
-        IntStream.range(0, numbers.length)
-                .mapToDouble(a -> numbers[a])
-                .forEach(a-> System.out.println(a));
-        return IntStream.range(0, numbers.length)
-                .mapToDouble(a -> numbers[a]).average().getAsDouble();
-
-
+interface   ArrayOperations {
+    static double getAverage(int[] numbers){
+        IntStream.range(0,numbers.length)
+                .mapToDouble(a->numbers[a]);
+        return IntStream.range(0,numbers.length)
+                .mapToDouble(a-> numbers[a]).average().getAsDouble();
     }
 }
+
+
+
+//public interface ArrayOperations {
+//    static  double getAverage(int[] numbers){
+//        IntStream.range(0, numbers.length)
+//                .mapToDouble(a -> numbers[a])
+//                .forEach(a-> System.out.println(a));
+//        return IntStream.range(0, numbers.length)
+//                .mapToDouble(a -> numbers[a]).average().getAsDouble();
+//
+//
+//    }
+//}
--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -5,18 +5,29 @@ import org.junit.jupiter.api.Test;
 
 public class ArrayOperationsTestSuite {
     @Test
-    void testGetAverage(){
+    void firstTestGetAverage(){
         //Given
         int[] muIntArray0 = {22,22,10,10};
-        int[] myIntArray1 = {10, 2, 8, 6, 7};
-        int[] myIntArray2 = {20};
         //Then
         double result0 = ArrayOperations.getAverage(muIntArray0);
-        double result1 = ArrayOperations.getAverage(myIntArray1);
-        double result2 = ArrayOperations.getAverage(myIntArray2);
         //When
         Assertions.assertEquals(16,result0 ,0);
+    }
+    void secondTestGetAverage(){
+        //Given
+        int[] myIntArray1 = {10, 2, 8, 6, 7};
+        //Then
+        double result1 = ArrayOperations.getAverage(myIntArray1);
+        //When
         Assertions.assertEquals(4.80, result1, 2);
+
+    }
+    void thirdTestGetAverage(){
+        //Given
+        int[] myIntArray2 = {20};
+        //Then
+        double result2 = ArrayOperations.getAverage(myIntArray2);
+        //When
         Assertions.assertEquals(20, result2, 0);
 
     }
