40e82f94a5cdbac685f3c717e18a7b3af8f6e353
Daniel Zak <D4nielZ4k@gmail.com>
2021-02-24 18:12:21 +0100

    commmit

--- kodilla-exception/src/test/java/com/kodilla/exception/test/FirstChallenge.java
+++ kodilla-exception/src/test/java/com/kodilla/exception/test/FirstChallenge.java
@@ -3,17 +3,10 @@ package com.kodilla.exception.test;
 public class FirstChallenge {
 
     public double divide(double a, double b) throws ArithmeticException {
-
-      try {
-          if (b == 0){
-              throw new ArithmeticException();
-          }
-      }catch (Exception e){
-          System.out.println("does not divide by 0");
-      }finally {
-          return a / b;
-      }
-
+        if (b == 0){
+            throw new ArithmeticException();
+        }
+        return a / b;
     }
 
     /**
@@ -22,11 +15,17 @@ public class FirstChallenge {
      */
     public static void main(String[] args) {
 
+
         FirstChallenge firstChallenge = new FirstChallenge();
+        try {
+            double result = firstChallenge.divide(3, 0);
+            System.out.println(result);
+        }catch (Exception e){
+            System.out.println("Does not divide by 0");
+        }finally {
+            System.out.println("First challenge");
+        }
 
-        double result = firstChallenge.divide(3, 0);
-
-        System.out.println(result);
 
     }
 }
