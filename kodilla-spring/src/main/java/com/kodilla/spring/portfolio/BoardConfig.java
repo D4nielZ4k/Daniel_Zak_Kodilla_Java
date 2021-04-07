f07d698f7d41314e6d428f2a9f8f8663bb55d70a
Daniel Zak <D4nielZ4k@gmail.com>
2021-04-07 20:34:29 +0200

    @Configuration

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
@@ -0,0 +1,36 @@
+package com.kodilla.spring.portfolio;
+
+
+import com.kodilla.spring.reader.Book;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class BoardConfig {
+
+   public Board board ;
+
+   @Bean(name = "getBoard")
+   public Board getBoard (){
+       return new Board(getTodoList(),getInProgressList(),getDoneList());
+   }
+
+
+   @Bean(name="toDoList")
+   public TaskList getTodoList (){
+       return new TaskList();
+    }
+
+    @Bean(name="InProgres")
+    public TaskList getInProgressList(){
+       return new TaskList();
+
+    }
+
+    @Bean(name="DoneList")
+    public TaskList getDoneList(){
+        return new TaskList();
+
+    }
+}
---
+++ kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
@@ -0,0 +1,35 @@
+package com.kodilla.spring.portfolio;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.Arrays;
+
+@SpringBootTest
+public class BoardTestSuite {
+
+
+    @Test
+    void testTaskAdd() {
+
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext(BoardConfig.class);
+        //When
+        Board board = (Board) context.getBean("getBoard");
+        board.toDoList.tasks.add("Task1");
+        board.inProgressList.tasks.add("Task2");
+        board.doneList.tasks.add("Task3");
+        //Then
+        Assertions.assertEquals("Task1", board.getToDoList().getTasks().get(0));
+        Assertions.assertEquals("Task2", board.getInProgressList().getTasks().get(0));
+        Assertions.assertEquals("Task3", board.getDoneList().getTasks().get(0));
+
+
+        Arrays.stream(context.getBeanDefinitionNames())
+                .forEach(System.out::println);
+    }
+
+}
