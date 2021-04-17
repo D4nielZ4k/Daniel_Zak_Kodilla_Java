24e56bca0fe6b963da4299a08c99b2fb4fc6654a
Daniel Zak <D4nielZ4k@gmail.com>
2021-04-17 17:32:41 +0200

    commit

--- kodilla-patterns/src/main/java/com/kodilla/patterns/strategy/predictors/AggressivePredictor.java
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/strategy/predictors/AggressivePredictor.java
@@ -6,5 +6,6 @@ public class AggressivePredictor implements BuyPredictor {
     @Override
     public String predictWhatToBuy() {
         return "[Aggressive predictor] Buy stock of XYZ";
+
     }
 }
